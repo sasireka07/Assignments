5ffd1ce2864224bdb0ffceff0a18d38ad004518e
sasireka07 <84171592+sasireka07@users.noreply.github.com>
2025-07-16 22:08:05 +0530

    Add files via upload

---
+++ Assignment14.java
@@ -0,0 +1,19 @@
+package assignments;
+
+public class Assignment14 {
+	double x = 2.00000;
+			int n = 10;
+			double y = 2.10000;int i = 3;
+			double z = 2.00000;int a = -2;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double result = Math.pow(2.00000, 10); 
+		System.out.println(result);
+		
+		double res = Math.pow(2.10000, 3); 
+		System.out.println(res); 
+		double r = Math.pow(2.00000, -2); 
+		System.out.println(r); 
+	}
+
+}
