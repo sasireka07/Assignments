680d0af8b64d39bc5dc0ba1d648a16a43d623817
sasireka07 <84171592+sasireka07@users.noreply.github.com>
2025-07-24 21:49:22 +0530

    updated assignment 15

--- Assignment15.java
+++ Assignment15.java
@@ -5,12 +5,14 @@ public class Assignment15 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String sentence = "Java programming is fun and challenging";
-
+		System.out.println("Total  no of words in the sentence is :"+sentence.length());
+		String uppercase=sentence.toLowerCase();
+		System.out.println("The first character of each word to uppercase:" +uppercase);
 		String result="";
 		String a[]=sentence.split(" ");
 		
 		for (int i =0;i <a.length; i++) {
-			System.out.println(a[i]+ " ");
+			//System.out.println(a[i]+ " ");
 
 		}
 		System.out.println("");
@@ -21,6 +23,7 @@ public class Assignment15 {
 		
 	}
 		System.out.println("The reverse word is :"+result);
+		
 	}
 
 
